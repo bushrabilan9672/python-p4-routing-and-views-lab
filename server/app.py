60a2b1f68c90739fcd8039a0a2794bbc328123d3
Your Name <your-email@example.com>
2026-06-04 11:03:22 +0300

    d

--- server/app.py
+++ server/app.py
@@ -4,5 +4,35 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<parameter>')
+def print_route(parameter):
+    print(parameter)
+    return parameter
+
+@app.route('/count/<int:parameter>')
+def count_route(parameter):
+    result = '\n'.join(str(i) for i in range(parameter))
+    if parameter > 0:
+        result += '\n'
+    return result
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math_route(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    return str(result)
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
